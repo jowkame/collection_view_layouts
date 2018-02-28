6cbd659d17591bab097c30de8a21e5218620bb5e
jowkame <>
2018-02-28 13:51:10 +0200

    Add content dynamic flow layout test

---
+++ Example/Tests/ContentDynamicFlowLayoutTest.swift
@@ -0,0 +1,31 @@
+// https://github.com/Quick/Quick
+
+import Quick
+import Nimble
+import collection_flow_layout
+
+class ContentDynamicFlowLayoutTest: QuickSpec {
+    override func spec() {
+        describe("Check base flow layout") {
+            let baseFlowLayout = ContentDynamicLayout()
+            
+            it("base flow layout fields validation") {
+                expect(baseFlowLayout.contentAlign).to(beAKindOf(DynamicContentAlign.self))
+                expect(baseFlowLayout.contentAlign).to(equal(DynamicContentAlign.left))
+                
+                expect(baseFlowLayout.cellsPadding).to(beAKindOf(ItemsPadding.self))
+                expect(baseFlowLayout.cellsPadding.horizontal).to(equal(0))
+                expect(baseFlowLayout.cellsPadding.vertical).to(equal(0))
+                
+                expect(baseFlowLayout.contentPadding).to(beAKindOf(ItemsPadding.self))
+                expect(baseFlowLayout.contentPadding.horizontal).to(equal(0))
+                expect(baseFlowLayout.contentPadding.vertical).to(equal(0))
+                
+                expect(baseFlowLayout.contentSize).to(beAKindOf(CGSize.self))
+                expect(baseFlowLayout.contentSize).to(equal(CGSize.zero))
+                
+                expect(baseFlowLayout.delegate).to(beNil())
+            }
+        }
+    }
+}
